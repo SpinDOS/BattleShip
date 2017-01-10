e8f2521c478cac75c13a41a63df9353f0cd961ed
Alexander <Alexander@ALEXANDER-PC>
2017-01-10 03:50:09 +0300

    Add exception handler to write to log

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -21,7 +21,32 @@ namespace BattleShip
         [STAThread]
         public static void Main(string[] args)
         {
-            new AppLifeCircle().Start();
+            // try start game
+            try
+            {
+                new AppLifeCircle().Start();
+            }
+            // handle any unhandled exceptions
+            catch (Exception exception)
+            {
+                // notify user
+                MessageBox.Show(
+                    @"Unexpected error occured. The application will be closed. You can try start new instance of the application",
+                    @"Unexpected error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // try write exception info to log file
+                try
+                {
+                    // write date, time, type of exception, inner data and empty new line
+                    StringBuilder content = new StringBuilder();
+                    content.AppendLine($"Date and time: {DateTime.Now.ToLongDateString()}, {DateTime.Now.ToLongTimeString()}");
+                    content.AppendLine($"Exception: {exception.GetType()}");
+                    content.AppendLine(JsonConvert.SerializeObject(exception, Formatting.Indented));
+                    content.AppendLine();
+                    // write to file
+                    File.AppendAllText("errors.log", content.ToString());
+                }
+                catch { /* ignored */ }
+            }
         }
     }
 }
